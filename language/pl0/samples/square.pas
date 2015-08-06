906125e7d3137d014f2cd625c08260914294aa17
yhirose <yuji.hirose.bug@gmail.com>
2015-08-05 23:06:58 -0400

    Added another sample for PL/0.

---
+++ language/pl0/samples/square.pas
@@ -0,0 +1,18 @@
+
+VAR x, squ;
+
+PROCEDURE square;
+BEGIN
+   squ:= x * x
+END;
+
+BEGIN
+   x := 1;
+   WHILE x <= 10 DO
+   BEGIN
+      CALL square;
+      ! squ;
+      x := x + 1
+   END
+END.
+
