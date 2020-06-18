ad1a6d27da8b21d3446e4995a66f807ea93a4ca6
peoro <peoro.noob@gmail.com>
2020-06-18 16:17:27 +0200

    Fix support to custom AST node types
    Fixes #116

--- peglib.h
+++ peglib.h
@@ -3937,7 +3937,7 @@ public:
   template <typename T = Ast> parser &enable_ast() {
     for (auto &x : *grammar_) {
       auto &rule = x.second;
-      if (!rule.action) { add_ast_action(rule); }
+      if (!rule.action) { add_ast_action<T>(rule); }
     }
     return *this;
   }
