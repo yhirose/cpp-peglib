5af245bec1ecf95b451fe808da809c30288fbbe8
yhirose <yuji.hirose.bug@gmail.com>
2026-08-09 19:38:00 -0400

    Release v1.16.0

--- peglib.h
+++ peglib.h
@@ -7,8 +7,8 @@
 
 #pragma once
 
-#define CPPPEGLIB_VERSION "1.15.1"
-#define CPPPEGLIB_VERSION_NUM "0x010f01"
+#define CPPPEGLIB_VERSION "1.16.0"
+#define CPPPEGLIB_VERSION_NUM "0x011000"
 
 /*
  * Configuration
